ec471ce5c03f0d672010138e80ac3969eb867f6a
Bishowjit-Ray <82141518+Bishowjit-Ray@users.noreply.github.com>
2022-09-25 15:45:15 +0600

    Add files via upload

---
+++ EnvironmentObjectBootcamp/EnvironmentObjectBootcamp/EnvironmentObjectBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvironmentObjectBootcampApp.swift
+//  EnvironmentObjectBootcamp
+//
+//  Created by Bishowjit Ray on 25/9/22.
+//
+
+import SwiftUI
+
+@main
+struct EnvironmentObjectBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
